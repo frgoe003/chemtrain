e7796e6e13729394e08ea250f3b8aff0a611d4b3
Paul <paul.fuchs@tum.de>
2024-09-30 19:53:04 +0200

    Drafted documentation

--- connector/connector/domain.h
+++ connector/connector/domain.h
@@ -9,31 +9,64 @@
 
 namespace jcn {
 
+	/**
+     * Contains all per-atom data of a domnain
+     */
     struct Atoms {
-        // This information is required to refine the shapes of the energy
-        // model
+        /**
+  		 * Maximum possible number of atoms in the domain, including local, ghost,
+         * and padded atoms.
+         */
         int n_atoms;
 
-        // The atom constructor keeps track wheter a recompilation of the
-        // mlir module is necessary
+        /**
+  		 * Tracks change of maxmimum number of atoms in the domain. If true,
+         * must re-compile the mlir module with new shapes.
+         */
         bool reallocate;
 
-        // These are pointers to the actual data
+        /** Positions literal */
         xla::Literal* positions;
+
+        /** Species literal */
         xla::Literal* species;
+
+        /** Ghost mask literal */
         xla::Literal* ghost_mask;
 
     };
 
+    /**
+     * Transforms atom data from the local domain into a padded XLA-copmpatible format
+     */
     class AtomBuilder {
     public:
+         /**
+          * @brief Constructor
+          * @param atom_multiplier Fraction of extra atoms to consider when re-allocate
+    	  *         the arrays
+          */
         AtomBuilder(float atom_multiplier) : max_atoms(0), atom_multiplier(atom_multiplier) {};
         ~AtomBuilder() = default;
 
-        // Padds the atom data to reduce number of recompilations
+        /**
+         * Padds the atom data to reduce number of recompilations
+         *
+         * @param inum Number of local atoms
+         * @param gnum Number of ghost atoms
+         * @param x Atom positions
+         * @param type Atom types (one-based species)
+         */
         Atoms build_domain(int inum, int gnum, double **x, int *type);
 
-        // Writes back the force to the original array and returns the potential
+        /**
+         * Writes back the force to the original array and returns the potential
+         *
+         * @param inum Number of local atoms
+         * @param f Target force array
+         * @param forces Forces from the XLA computation
+         * @param potential Potential from the XLA computation
+         */
         double evaluate_domain(int inum, double **f, std::shared_ptr<xla::Literal> forces, std::shared_ptr<xla::Literal> potential);
 
     private:
