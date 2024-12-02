0c58e73a74d2f4a0dcea0a013b11d58c43c02afc
Paul <paul.fuchs@tum.de>
2024-12-02 09:42:18 +0100

    Corrected model and training

--- jax_md_mod/custom_electrostatics.py
+++ jax_md_mod/custom_electrostatics.py
@@ -119,10 +119,10 @@ def charge_eq_energy_neighborlist(displacement, r_onset, r_cutoff, interaction="
         else:
             raise ValueError(f"Unknown method {method}")
 
-        # qeq_energy = total_energy_fn(position, neighbor, charge=charges, radii=radii, chi=chi, idmp=idmp)
+        qeq_energy = total_energy_fn(position, neighbor, charge=charges, radii=radii, chi=chi, idmp=idmp)
 
         # Only include electrostatic energy
-        qeq_energy = total_energy_fn(position, neighbor, charge=charges, radii=radii)
+        # qeq_energy = total_energy_fn(position, neighbor, charge=charges, radii=radii)
         return qeq_energy, charges
 
     return energy_fn
