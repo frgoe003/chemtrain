bd828fc5d09ea9470ec1b1146c0994e6aafaa5e7
Paul Fuchs <paul.fuchs@tum.de>
2026-01-19 09:51:55 +0100

    Simplified custom_vmap rule

--- chemtrain/compose/utils.py
+++ chemtrain/compose/utils.py
@@ -86,85 +86,37 @@ def batch_apply_fn(_apply_fn: ApplyFn) -> ApplyFn:
     
     @wrapped_fun_bwd.def_vmap
     def wrapped_fun_bwd_batch(
-            axis_size, in_batched, res, b_y_bar):
+            axis_size, in_batched, res, y_bar):
         
         args_batched, y_bar_batched = in_batched
-        bparams, *inputs_batched = args_batched
-        params, *graph_args = res
-        
-        flattened_graph, graph_shapes = flatten_graph(
-            axis_size, inputs_batched, *graph_args
-        )
-        senders_flat, receivers_flat, edge_features_flat, node_features_flat = \
-            flattened_graph
-        num_graphs, num_edges, natoms = graph_shapes
+        # bparams, *_, bedge_features, bnode_features = args_batched
  
         if not y_bar_batched:
-            # Tile the cotangent if it is not batched
-            if b_y_bar.shape[0] == natoms: 
-                y_bar_flat = jnp.tile(b_y_bar, (num_graphs,))
-            else:
-                y_bar_flat = b_y_bar
-        else:
-            y_bar_flat = jnp.reshape(b_y_bar, (-1,))
+            y_bar = jnp.tile(y_bar[jnp.newaxis, :], (axis_size, 1))
 
-        # TODO: Can we also just apply the vjp to the batched fwd function to
-        #       save some operations?
-        
-        # TODO: Correct the param batching logic here....
-        #       Params could in principle be batched INSIDE the jvp function.
-
-        # Let jax figure out the vjp on the flattened supergraph
-
-        if jax.tree.leaves(bparams)[0]:
-            _, vjp_fn = jax.vjp(
-                lambda *args: lax.map(
-                    lambda p: wrapped_apply_fn(p, *args[1:]), args[0]
-                ),
-                params, senders_flat, receivers_flat, edge_features_flat,
-                node_features_flat
-            )
-        else:
-            _, vjp_fn = jax.vjp(
-                apply_fn, params, senders_flat, 
-                receivers_flat, edge_features_flat, node_features_flat
-            )
-
-        grads = vjp_fn(y_bar_flat)
-        
-        g_params, g_senders, g_receivers, g_edge_features, g_node_features = grads
-
-        # Reshape the gradients back to the batched shape
-        out_grads = (
-            g_params,
-            g_senders.reshape((num_graphs, num_edges)),
-            g_receivers.reshape((num_graphs, num_edges)),
-            tuple(
-                g_edge_feat.reshape((num_graphs, num_edges) + g_edge_feat.shape[1:])
-                for g_edge_feat in g_edge_features
-            ),
-            tuple(
-                g_node_feat.reshape((num_graphs, natoms) + g_node_feat.shape[1:])
-                for g_node_feat in g_node_features
-            )
+        # The batched function figures out batching of parameters and graphs
+        _, vjp_fn = jax.vjp(
+            lambda *args: wrapped_fun_batch(
+                axis_size, args_batched, *args, compute_vjp=True
+            )[0], *res
         )
 
-        # Return which of the outputs are batched
-        out_batched = (
-            bparams, True, True,
-            (True,) * len(g_edge_features),
-            (True,) * len(g_node_features)
-        )
+        grads = vjp_fn(y_bar)
 
-        return out_grads, out_batched
+        return grads, args_batched
 
-    wrapped_apply_fn.defvjp(wrapped_fun_fwd, wrapped_fun_bwd)
-
-    @wrapped_apply_fn.def_vmap
     def wrapped_fun_batch(
-            axis_size, in_batched, params, senders, receivers, edge_features, node_features):
+            axis_size, in_batched, params, senders, receivers, edge_features,
+            node_features, compute_vjp=False):
 
         bparams, *inputs_batched = in_batched
+
+        # We must avoid the custom_vmap decorated function, as no vjp rule
+        # is defined for it.
+        if compute_vjp:
+            fn = apply_fn
+        else:
+            fn = wrapped_apply_fn
 
         # Find out whether any of the parameters is batched
         batched_params = jax.tree.reduce(
@@ -187,28 +139,24 @@ def batch_apply_fn(_apply_fn: ApplyFn) -> ApplyFn:
             )
 
             energies = lax.map(
-                lambda args: wrapped_apply_fn(*args), args_tiled
+                lambda args: fn(*args), args_tiled
             )
 
             return energies, True
         else:
             
-            flattened_graph, graph_shapes = flatten_graph(
+            flattened_graph = flatten_graph(
                 axis_size, inputs_batched, senders, receivers, edge_features,
                 node_features
             )
-            senders, receivers, edge_features_flat, node_features_flat = \
-                flattened_graph
-            num_graphs, num_edges, natoms = graph_shapes
 
-
-            energies_flat = wrapped_apply_fn(
-                params, senders, receivers, edge_features_flat,
-                node_features_flat
-            )
+            energies_flat = fn(params, *flattened_graph)
 
             # Unflatten the results
             return energies_flat.reshape((axis_size, -1)), True
+
+    wrapped_apply_fn.defvjp(wrapped_fun_fwd, wrapped_fun_bwd)
+    wrapped_apply_fn.def_vmap(wrapped_fun_batch)
 
     return wrapped
 
@@ -277,4 +225,4 @@ def flatten_graph(axis_size, in_batched, senders, receivers, edge_features, node
                     feat[None, :], (num_graphs, 1) + (1,) * (feat.ndim -1)
                 ).ravel(),)
 
-        return (senders, receivers, edge_features_flat, node_features_flat), (num_graphs, num_edges, natoms)
+        return senders, receivers, edge_features_flat, node_features_flat
